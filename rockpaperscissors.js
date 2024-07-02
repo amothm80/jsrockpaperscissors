196675b81498cb24c0b26b7ca3cc0e2c275b39e4
amothm80 <amo.1980@gmail.com>
2024-07-02 22:38:37 +0300

    completed RPS UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -24,14 +24,14 @@ function playRound(humanChoice, computerChoice){
         case "rock":
             switch(humanChoice){
                 case "rock":
-                    console.log(`${humanChoice} and ${computerChoice} is a tie`);
+                    result = `${humanChoice} and ${computerChoice} is a tie`;
                     break;
                 case "paper":
-                    console.log(`human wins, ${humanChoice} beats ${computerChoice}`);
+                    result = `human wins, ${humanChoice} beats ${computerChoice}`;
                     humanScore++;
                     break;
                 case "scissors":
-                    console.log(`computer wins, ${computerChoice} beats ${humanChoice}`);
+                    result = `computer wins, ${computerChoice} beats ${humanChoice}`;
                     computerScore++;
                     break;
                 default:
@@ -41,14 +41,14 @@ function playRound(humanChoice, computerChoice){
         case "paper":
             switch(humanChoice){
                 case "rock":
-                    console.log(`computer wins, ${computerChoice} beats ${humanChoice}`);
+                    result = `computer wins, ${computerChoice} beats ${humanChoice}`;
                     computerScore++;
                     break;
                 case "paper":
-                    console.log(`${humanChoice} and ${computerChoice} is a tie`);
+                    result = `${humanChoice} and ${computerChoice} is a tie`;
                     break;
                 case "scissors":
-                    console.log(`human wins, ${humanChoice} beats ${computerChoice}`);
+                    result = `human wins, ${humanChoice} beats ${computerChoice}`;
                     humanScore++;
                     break;
                 default:
@@ -58,33 +58,104 @@ function playRound(humanChoice, computerChoice){
         case "scissors":
             switch(humanChoice){
                 case "rock":
-                    console.log(`human wins, ${humanChoice} beats ${computerChoice}`);
+                    result = `human wins, ${humanChoice} beats ${computerChoice}`;
                     humanScore++;
                     break;
                 case "paper":
-                    console.log(`computer wins, ${computerChoice} beats ${humanChoice}`);
+                    result = `computer wins, ${computerChoice} beats ${humanChoice}`;
                     computerScore++;
                     break;
                 case "scissors":
-                    console.log(`${humanChoice} and ${computerChoice} is a tie`);
+                    result = `${humanChoice} and ${computerChoice} is a tie`;
                     break;
                 default:
                     break;
             }            
             break;
     }
+    if ( humanScore == 5 ){
+        alert("Human Wins The Game");
+        humanScore = 0;
+        computerScore = 0;
+    }
+    if ( computerScore == 5 ){
+        alert("Computer Wins The Game");
+        humanScore = 0;
+        computerScore = 0;
+    }
+    let rT = document.querySelector("#resultText");
+    rT.textContent = result;
+    let hS = document.querySelector("#humanScore");
+    hS.textContent = `Human score is ${humanScore}`;
+    let cS = document.querySelector("#computerScore");
+    cS.textContent = `Computer score is ${computerScore}`;
+
+
 }
 
 
 function playGame(){
-    humanScore = 0;
-    computerScore = 0;
 
-    for (let i = 0; i<5; i++){
-        playRound(getHumanChoice(),getComputerChoice())
-    }
+
+
+
+
+    // for (let i = 0; i<5; i++){
+    //     playRound(getHumanChoice(),getComputerChoice())
+    // }
     console.log(`human score ${humanScore}`)
     console.log(`computer score ${computerScore}`)
 }
 
-playGame()
+
+let humanScore = 0;
+let computerScore = 0;
+
+let body = document.querySelector("body");
+let rockButton = document.createElement("button");
+rockButton.setAttribute("id","rock")
+rockButton.textContent = "ROCK";
+let paperButton = document.createElement("button");
+paperButton.setAttribute("id","paper");
+paperButton.textContent = "PAPER"
+let scissorsButton = document.createElement("button");
+scissorsButton.setAttribute("id","scissors");
+scissorsButton.textContent = "SCISSORS";
+let br = document.createElement("br");
+let resultText = document.createElement("p");
+resultText.setAttribute("id","resultText")
+resultText.style.border = "solid";
+let humanScoreEl = document.createElement("p");
+humanScoreEl.setAttribute("id","humanScore")
+humanScoreEl.style.border = "solid";
+let computerScoreEl = document.createElement("p");
+computerScoreEl.setAttribute("id","computerScore")
+computerScoreEl.style.border = "solid";
+
+
+body.appendChild(rockButton);
+body.appendChild(br);
+body.appendChild(paperButton)
+body.appendChild(br)
+body.appendChild(scissorsButton);
+body.appendChild(br);
+body.appendChild(resultText);
+body.appendChild(br);
+body.appendChild(humanScoreEl);
+body.appendChild(br);
+body.appendChild(computerScoreEl);
+body.appendChild(br);
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click",() =>{
+        playRound(button.getAttribute("id"),getComputerChoice());
+
+    })
+}
+)
+
+
+
+
+//playGame()
